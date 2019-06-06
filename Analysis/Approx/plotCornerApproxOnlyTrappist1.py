8feab7876ce9c11a162dd641e4c5e6d2b9062d39
David Fleming <dflemin3@uw.edu>
2019-06-06 12:03:47 -0700

    added converged AP outputs

--- Analysis/Approx/plotCornerApproxOnlyTrappist1.py
+++ Analysis/Approx/plotCornerApproxOnlyTrappist1.py
@@ -56,8 +56,8 @@ mpl.rc('font',**{'family':'serif'})
 mpl.rc('text', usetex=True)
 
 # Path to data
-filename = "../../Data/convergedAP.h5"
-#filename = "../../Data/apRun2.h5"
+#filename = "../../Data/convergedAP.h5"
+filename = "../../Data/apRun9.h5"
 
 # Extract data
 samples = extractMCMCResults(filename, blobsExist=False, burn=500)
